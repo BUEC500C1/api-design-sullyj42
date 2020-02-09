3485fdcfd2d0a03bf8584010594274393b8bab46
ECE Student <sullyj42@gmail.com>
2020-02-09 13:29:51 -0500

    Documented run script

--- runcommand.sh
+++ runcommand.sh
@@ -1,4 +1,5 @@
-python tweet_import.py;
-python tweet_import.py realdonaldtrump;
-python tweet_import.py potus;
+pip install -r requirements.txt
+python tweet_import.py; # Runs with default username, brianna abbott
+python tweet_import.py realdonaldtrump; # Runs with Trump's personal twitter
+python tweet_import.py potus; # Runs with current POTUS
 
